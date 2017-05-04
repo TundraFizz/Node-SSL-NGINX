3f756e539167fc5664e713f597f1d564750b23ae
Tundra Fizz <MageLeif@Yahoo.com>
2017-05-04 16:01:40 -0700

    Change the SSL Ciphers

--- main.js
+++ main.js
@@ -157,35 +157,35 @@ SSLNginx.prototype.RoundOne = function(){
 };
 
 SSLNginx.prototype.RoundTwo = function(){
-  return `\n`                                                                        +
-  `server {\n`                                                                       +
-  `  listen 9000;\n`                                                                 +
-  `  listen 443 ssl;\n`                                                              +
-  `\n`                                                                               +
-  `  server_name ${this.domainName};\n`                                              +
-  `\n`                                                                               +
-  `  if ($scheme = http) {return 301 https://$server_name$request_uri;}\n`           +
-  `\n`                                                                               +
-  `  ssl_certificate     /etc/letsencrypt/live/${this.projectName}/fullchain.pem;\n` +
-  `  ssl_certificate_key /etc/letsencrypt/live/${this.projectName}/privkey.pem;\n`   +
-  `\n`                                                                               +
-  `  ssl_protocols TLSv1 TLSv1.1 TLSv1.2;\n`                                         +
-  `  ssl_prefer_server_ciphers on;\n`                                                +
-  `  ssl_ciphers "EECDH+AESGCM:EDH+AESGCM:AES256+EECDH:AES256+EDH";\n`               +
-  `  ssl_ecdh_curve secp384r1;\n`                                                    +
-  `  ssl_session_cache shared:SSL:10m;\n`                                            +
-  `  ssl_session_tickets off;\n`                                                     +
-  `  ssl_stapling on;\n`                                                             +
-  `  ssl_stapling_verify on;\n`                                                      +
-  `  resolver 8.8.8.8 8.8.4.4 valid=300s;\n`                                         +
-  `  resolver_timeout 5s;\n`                                                         +
-  `  add_header Strict-Transport-Security "max-age=63072000; includeSubdomains";\n`  +
-  `  add_header X-Frame-Options DENY;\n`                                             +
-  `  add_header X-Content-Type-Options nosniff;\n`                                   +
-  `\n`                                                                               +
-  `  ssl_dhparam /etc/letsencrypt/live/dhparam.pem;\n`                               +
-  `\n`                                                                               +
-  `  location / {proxy_pass http://${this.ip}:${this.port};}\n`                      +
+  return `\n`                                                                                                                        +
+  `server {\n`                                                                                                                       +
+  `  listen 9000;\n`                                                                                                                 +
+  `  listen 443 ssl;\n`                                                                                                              +
+  `\n`                                                                                                                               +
+  `  server_name ${this.domainName};\n`                                                                                              +
+  `\n`                                                                                                                               +
+  `  if ($scheme = http) {return 301 https://$server_name$request_uri;}\n`                                                           +
+  `\n`                                                                                                                               +
+  `  ssl_certificate     /etc/letsencrypt/live/${this.projectName}/fullchain.pem;\n`                                                 +
+  `  ssl_certificate_key /etc/letsencrypt/live/${this.projectName}/privkey.pem;\n`                                                   +
+  `\n`                                                                                                                               +
+  `  ssl_protocols TLSv1 TLSv1.1 TLSv1.2;\n`                                                                                         +
+  `  ssl_prefer_server_ciphers on;\n`                                                                                                +
+  `  ssl_ciphers "ssl_ciphers ECDH+AESGCM:DH+AESGCM:ECDH+AES256:DH+AES256:ECDH+AES128:DH+AES:RSA+AESGCM:RSA+AES:!aNULL:!MD5:!DSS;\n` +
+  `  ssl_ecdh_curve secp384r1;\n`                                                                                                    +
+  `  ssl_session_cache shared:SSL:10m;\n`                                                                                            +
+  `  ssl_session_tickets off;\n`                                                                                                     +
+  `  ssl_stapling on;\n`                                                                                                             +
+  `  ssl_stapling_verify on;\n`                                                                                                      +
+  `  resolver 8.8.8.8 8.8.4.4 valid=300s;\n`                                                                                         +
+  `  resolver_timeout 5s;\n`                                                                                                         +
+  `  add_header Strict-Transport-Security "max-age=63072000; includeSubdomains";\n`                                                  +
+  `  add_header X-Frame-Options DENY;\n`                                                                                             +
+  `  add_header X-Content-Type-Options nosniff;\n`                                                                                   +
+  `\n`                                                                                                                               +
+  `  ssl_dhparam /etc/letsencrypt/live/dhparam.pem;\n`                                                                               +
+  `\n`                                                                                                                               +
+  `  location / {proxy_pass http://${this.ip}:${this.port};}\n`                                                                      +
   `}\n`;
 };
 
